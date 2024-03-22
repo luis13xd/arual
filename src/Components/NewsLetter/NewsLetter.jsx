563b103ac3805d39d58af2fff361cc4ce273bd46
Luis <trujillolopezluisxd@gmail.com>
2024-03-22 11:58:50 -0500

    responsive en seccion de subscripcion

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -4,12 +4,14 @@ import './NewsLetter.css';
 export const NewsLetter = () => {
   return (
     <div className='newsletter'>
+      <div className="newsletter-main">
         <h1>Recibe ofertas exclusivas por Correo</h1>
         <p>Suscribete para estar actualizado de la moda</p>
-        <div>
-            <input type="email" placeholder='Correo electronico' />
-            <button>Suscribir</button>
+        <div className='newsletter-div'>
+          <input type="email" placeholder='Correo electronico' />
+          <button>Suscribir</button>
         </div>
+      </div>
     </div>
   )
 }
