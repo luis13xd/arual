e0dfd3b83db369508759ded3e7a5d4fe6b2b6f95
Luis <trujillolopezluisxd@gmail.com>
2025-02-09 04:06:50 -0500

    nuevos peroductos 14

--- src/Components/Assets/new_collections.js
+++ src/Components/Assets/new_collections.js
@@ -1,11 +1,8 @@
 import shorttiedyeverde1 from "./lyf/shorttiedyeverde1.jpeg";
 import shorttiedyeazulrosado1 from "./lyf/shorttiedyeazulrosado1.jpeg";
 import shorttiedyenaranjaamarillo1 from "./lyf/shorttiedyenaranjaamarillo1.jpeg";
-import shorttiedyerosado1 from "./lyf/shorttiedyerosado1.jpeg";
 import bodybambieamarillo1 from "./body/bodybambieamarillo1.jpeg";
 import bodybambienegro1 from "./body/bodybambienegro1.jpeg";
-import bodyindigo2 from "./body/bodyindigo2.jpeg";
-import chaquetamilano1 from "./body/chaquetamilano1.jpeg";
 
 let new_collections = [
   {
@@ -30,18 +27,11 @@ let new_collections = [
     old_price: 60000,
   },
   {
-    id: 205,
-    name: "SHORT TIE DYE - TONO ROSADO",
-    image: shorttiedyerosado1,
+    id: 206,
+    name: "SHORT TIE DYE - TONO VERDE",
+    image: shorttiedyeverde1,
     new_price: 40000,
     old_price: 60000,
-  },
-  {
-    id: 302,
-    name: "BODY INDIGO - TONO NEGRO",
-    image: bodyindigo2,
-    new_price: 75000,
-    old_price: 90000,
   },
   {
     id: 301,
@@ -57,13 +47,7 @@ let new_collections = [
     new_price: 75000,
     old_price: 90000,
   },
-  {
-    id: 307,
-    name: "CHAQUETA MILANO",
-    image: chaquetamilano1,
-    new_price: 75000,
-    old_price: 90000,
-  },
+
 ];
 
 export default new_collections;
