666d3451c8d8bf6ab692b63e8ecd5ed61dff952c
Luis <trujillolopezluisxd@gmail.com>
2026-01-09 22:05:16 -0500

    agregamos producto en nueva coleccion

--- src/Components/Assets/new_collections.js
+++ src/Components/Assets/new_collections.js
@@ -1,5 +1,5 @@
 import topemmanegro1 from "./body/topemmanegro1.jpeg";
-import bodybambienegro1 from "./body/bodybambienegro1.jpeg";
+import topemmablanco1 from "./body/topemmablanco1.jpeg";
 import topsamalia1 from "./body/topsamalia1.jpeg";
 import topzaharaazulmorado1 from "./body/topzaharaazulmorado1.jpeg";
 
@@ -15,20 +15,9 @@ let new_collections = [
     id: 305,
     name: "TOPS EMMA - TONO BLANCO",
     category: "bodysytops",
-    description: "Tela completamente acanalada",
     image: topemmablanco1,
-    image2: topemmablanco2,
-    image3: topemmablanco3,
-    image4: topemmablanco4,
     new_price: 40000,
     old_price: 50000,
-    sizes: {
-      XS: false,
-      S: true,
-      M: true,
-      L: false,
-      XL: false,
-    },
   },
   {
     id: 306,
