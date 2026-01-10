13c77e02c95f3565bcb668fe537af03f0f9afb91
Luis <trujillolopezluisxd@gmail.com>
2026-01-09 22:10:14 -0500

    dimos formato a codigo

--- src/Components/Assets/new_collections.js
+++ src/Components/Assets/new_collections.js
@@ -4,14 +4,14 @@ import topsamalia1 from "./body/topsamalia1.jpeg";
 import topzaharaazulmorado1 from "./body/topzaharaazulmorado1.jpeg";
 
 let new_collections = [
-{
+  {
     id: 304,
     name: "TOPS EMMA - TONO NEGRO",
     image: topemmanegro1,
     new_price: 40000,
     old_price: 50000,
   },
-{
+  {
     id: 305,
     name: "TOPS EMMA - TONO BLANCO",
     category: "bodysytops",
@@ -33,7 +33,6 @@ let new_collections = [
     new_price: 25000,
     old_price: 40000,
   },
-
 ];
 
 export default new_collections;
