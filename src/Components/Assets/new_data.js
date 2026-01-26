786bd800244fa5628a284d6b63e58c724600fd2f
Luis <trujillolopezluisxd@gmail.com>
2026-01-26 08:50:54 -0500

    eliminamos set pink

--- src/Components/Assets/new_data.js
+++ src/Components/Assets/new_data.js
@@ -1,16 +1,8 @@
 import setbabyblue1 from "./sets/setbabyblue1.jpeg";
 import setmarazul1 from "./sets/setmarazul1.jpeg";
-import setpink1 from "./sets/setpink1.jpeg";
 import enterizoflexazulcielo1 from "./enterizos/enterizoflexazul1.jpeg";
 
 let new_data = [
-  {
-    id: 408,
-    name: "ENTERIZO PINK - TONO ROSADO",
-    image: setpink1,
-    new_price: 80000,
-    old_price: 120000,
-  },
   {
     id: 402,
     name: "SET BABY - TONO AZUL",
