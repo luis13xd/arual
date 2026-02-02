3daa3a2d0e904b667cb488dab79ca92eddb77af6
Luis <trujillolopezluisxd@gmail.com>
2026-02-01 21:51:36 -0500

    ajusamos code en decription

--- src/Components/DescripioBox/DescriptioBox.jsx
+++ src/Components/DescripioBox/DescriptioBox.jsx
@@ -1,23 +1,26 @@
-import './DescriptioBox.css';
+import "./DescriptioBox.css";
 
 export const DescriptioBox = () => {
   return (
-    <div className='descriptionbox'>
-        <div className="descriptionbox-navigator">
-            <div className="descriptionbox-nav-box">
-                Descripción
-            </div>
-            <div className="descriptionbox-nav-box fade">
-                Vistas (103)
-            </div>
-        </div>
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Descripción</div>
+        <div className="descriptionbox-nav-box fade">Vistas (103)</div>
+      </div>
 
-        <div className="descriptionbox-description">
-            <p>
-            Esta prenda deportiva está confeccionada en tela acanalada de alta elasticidad, diseñada para ofrecer un ajuste ceñido y cómodo. Su estructura acanalada no solo proporciona soporte, sino que también resalta la figura de manera estilizada. Cuenta con un efecto push-up en la zona de los glúteos, gracias a un diseño de costuras estratégicas que levantan y realzan la forma natural, creando una silueta más definida. Ideal para ejercicios que requieren libertad de movimiento, esta prenda combina funcionalidad y estilo, ofreciendo soporte y confort durante todo el entrenamiento.    
-             </p>
-        </div>
-
+      <div className="descriptionbox-description">
+        <p>
+          Esta prenda deportiva está confeccionada en tela acanalada de alta
+          elasticidad, diseñada para ofrecer un ajuste ceñido y cómodo. Su
+          estructura acanalada no solo proporciona soporte, sino que también
+          resalta la figura de manera estilizada. Cuenta con un efecto push-up
+          en la zona de los glúteos, gracias a un diseño de costuras
+          estratégicas que levantan y realzan la forma natural, creando una
+          silueta más definida. Ideal para ejercicios que requieren libertad de
+          movimiento, esta prenda combina funcionalidad y estilo, ofreciendo
+          soporte y confort durante todo el entrenamiento.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
