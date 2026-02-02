70eb3db1efa02d9f0b4618622cda84bc24f0216d
Luis <trujillolopezluisxd@gmail.com>
2026-02-01 21:52:13 -0500

    ajusamos code en footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,33 +1,33 @@
-import './Footer.css';
-import instagram_icon from '../Assets/instagram_icon.png';
-import pinteres_icon from '../Assets/pintester_icon.png';
-import whatsapp_icon from '../Assets/whatsapp_icon.png';
+import "./Footer.css";
+import instagram_icon from "../Assets/instagram_icon.png";
+import pinteres_icon from "../Assets/pintester_icon.png";
+import whatsapp_icon from "../Assets/whatsapp_icon.png";
 
 export const Footer = () => {
   return (
-    <div className='footer'>
-        <div className="footer-logo">
-            <p>ARUAL</p>
+    <div className="footer">
+      <div className="footer-logo">
+        <p>ARUAL</p>
+      </div>
+      <ul className="footer-links">
+        <li>Contáctanos</li>
+      </ul>
+      <div className="footer-social-icon">
+        <div className="footer-icons-container">
+          <img src={instagram_icon} alt="i" />
         </div>
-        <ul className="footer-links">
-            <li>Contáctanos</li>
-        </ul>
-        <div className="footer-social-icon">
-            <div className="footer-icons-container">
-                <img src={instagram_icon} alt="i" />
-            </div>
-            <div className="footer-icons-container">
-                <img src={pinteres_icon} alt="p" />
-            </div>
-            <div className="footer-icons-container">
-                <img src={whatsapp_icon} alt="w" />
-            </div>
+        <div className="footer-icons-container">
+          <img src={pinteres_icon} alt="p" />
         </div>
-        <div className="footer-copyright">
-            <hr />
-            <p>Copyright @ 2025 - Todos los Derechos Reservados</p>
-            <h3>ARUAL S.A.S</h3>
+        <div className="footer-icons-container">
+          <img src={whatsapp_icon} alt="w" />
         </div>
+      </div>
+      <div className="footer-copyright">
+        <hr />
+        <p>Copyright @ 2025 - Todos los Derechos Reservados</p>
+        <h3>ARUAL S.A.S</h3>
+      </div>
     </div>
-  )
-}
+  );
+};
