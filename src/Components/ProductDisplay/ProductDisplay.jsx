345522ce935de71c21ee17b70389926c8093a5ae
Luis <trujillolopezluisxd@gmail.com>
2026-02-23 21:18:47 -0500

    ajustamos codigo en related product

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -21,7 +21,6 @@ export const ProductDisplay = (props) => {
     }).format(price);
   };
   
-
   const handleSizeSelection = (size) => {
     if (product.sizes[size]) {
       setSelectedSize(size);
