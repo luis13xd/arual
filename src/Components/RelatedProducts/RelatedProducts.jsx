ba31260bc08c3285e947402e17b4b027bd9a884d
Luis <trujillolopezluisxd@gmail.com>
2026-02-23 21:16:47 -0500

    ajustamos codigo en related product

--- src/Components/RelatedProducts/RelatedProducts.jsx
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import './RelatedProducts.css';
-import data_product from '../Assets/data';
-import { Item } from '../Item/Item';
+import React from "react";
+import "./RelatedProducts.css";
+import data_product from "../Assets/data";
+import { Item } from "../Item/Item";
 
 export const RelatedProducts = () => {
-    return (
-        <div className='relatedproducts'>
-            <h2>Más Productos</h2>
-            <hr />
-            <div className="relatedproducts-item">
-                {data_product.map((item,i) => {
-                    return <Item
-                                key={i}
-                                id={item.id}
-                                name={item.name}
-                                image={item.image}
-                                new_price={item.new_price}
-                                old_price={item.old_price}
-                                offer={item.offer}
-                            />
-                })}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="relatedproducts">
+      <h2>Más Productos</h2>
+      <hr />
+      <div className="relatedproducts-item">
+        {data_product.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+              offer={item.offer}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
