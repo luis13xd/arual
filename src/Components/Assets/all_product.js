43e186323e96a87818f16fa8bbcf6a298e02c55e
Luis <trujillolopezluisxd@gmail.com>
2026-05-10 03:58:45 -0500

    eliminamos set emily tono negro

--- src/Components/Assets/all_product.js
+++ src/Components/Assets/all_product.js
@@ -144,11 +144,6 @@ import setmarie1 from "./sets/setmarie1.jpeg";
 import setmarie2 from "./sets/setmarie2.jpeg";
 import setmarie3 from "./sets/setmarie3.jpeg";
 import setmarie4 from "./sets/setmarie4.jpeg";
-// 404
-import setemilinegro1 from "./sets/setemilinegro1.jpeg";
-import setemilinegro2 from "./sets/setemilinegro2.jpeg";
-import setemilinegro3 from "./sets/setemilinegro3.jpeg";
-import setemilinegro4 from "./sets/setemilinegro4.jpeg";
 // 407
 import setmilanborgoña1 from "./sets/setmilanborgoña1.jpeg";
 import setmilanborgoña2 from "./sets/setmilanborgoña2.jpeg";
@@ -755,27 +750,6 @@ let all_product = [
     new_price: 90000,
     old_price: 100000,
     offer: false,
-    sizes: {
-      XS: false,
-      S: true,
-      M: false,
-      L: false,
-      XL: false,
-    },
-  },
-  {
-    id: 404,
-    name: "SET EMILY - TONO NEGRO",
-    category: "sets",
-    description:
-      "Tela completamente acanalada, su efecto push up te hará lucir increible",
-    image: setemilinegro1,
-    image2: setemilinegro2,
-    image3: setemilinegro3,
-    image4: setemilinegro4,
-    new_price: 100000,
-    old_price: 130000,
-    offer: true,
     sizes: {
       XS: false,
       S: true,
