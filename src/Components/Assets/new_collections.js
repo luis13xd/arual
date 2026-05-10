a58477bd80da65f0b964ec2ab9d8a94b06d1414d
Luis <trujillolopezluisxd@gmail.com>
2026-05-10 04:25:43 -0500

    eliminamos 2 tops

--- src/Components/Assets/new_collections.js
+++ src/Components/Assets/new_collections.js
@@ -1,22 +1,14 @@
-import topemmanegro1 from "./body/topemmanegro1.jpeg";
-import topemmablanco1 from "./body/topemmablanco1.jpeg";
+//import topemmanegro1 from "./body/topemmanegro1.jpeg";
 
 let new_collections = [
-  {
-    id: 304,
-    name: "TOPS EMMA - TONO NEGRO",
-    image: topemmanegro1,
-    new_price: 40000,
-    old_price: 50000,
-  },
-  {
-    id: 305,
-    name: "TOPS EMMA - TONO BLANCO",
-    category: "bodysytops",
-    image: topemmablanco1,
-    new_price: 40000,
-    old_price: 50000,
-  },
+  // {
+  //   id: 304,
+  //   name: "TOPS EMMA - TONO NEGRO",
+  //   image: topemmanegro1,
+  //   new_price: 40000,
+  //   old_price: 50000,
+  // },
+
 ];
 
 export default new_collections;
