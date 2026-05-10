9d9e059dbe297be58958ad8484cfd1635a86ca56
Luis <trujillolopezluisxd@gmail.com>
2026-05-10 04:58:34 -0500

    actalizamos nueva collecion

--- src/Components/Assets/new_collections.js
+++ src/Components/Assets/new_collections.js
@@ -1,14 +1,23 @@
-//import topemmanegro1 from "./body/topemmanegro1.jpeg";
+import topsilverazul1 from "./body/topsilverazul1.jpeg";
+import topsemma1 from "./body/topsemma1.jpeg";
 
 let new_collections = [
-  // {
-  //   id: 304,
-  //   name: "TOPS EMMA - TONO NEGRO",
-  //   image: topemmanegro1,
-  //   new_price: 40000,
-  //   old_price: 50000,
-  // },
-
+  {
+    id: 303,
+    name: "TOPS EMMA - TONO BLANCO-NEGRO-ROJO-CAFÉ-ROSADO",
+    image: topsemma1,
+    new_price: 45000,
+    old_price: 50000,
+    offer: false,
+  },
+  {
+    id: 308,
+    name: "TOPS SILBER - TONO AZUL",
+    image: topsilverazul1,
+    new_price: 30000,
+    old_price: 60000,
+    offer: true,
+  },
 ];
 
 export default new_collections;
