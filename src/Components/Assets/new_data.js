df52aea9982336bdfc2a5c8e9d4fbb3dda2cb7b5
Luis <trujillolopezluisxd@gmail.com>
2026-05-13 13:44:51 -0500

    eliminams imgs de sets

--- src/Components/Assets/new_data.js
+++ src/Components/Assets/new_data.js
@@ -1,6 +1,4 @@
 import enterizoflexazulcielo1 from "./enterizos/enterizoflexazul1.jpeg";
-import setlimaamarillo1 from "./sets/setlimaamarillo1.jpeg";
-import setmilanborgoña1 from "./sets/setmilanborgoña1.jpeg";
 
 let new_data = [
   {
@@ -12,22 +10,7 @@ let new_data = [
     old_price: 140000,
     offer: false,
   },
-  {
-    id: 401,
-    name: "SET LIMA - TONO AMARILLO",
-    image: setlimaamarillo1,
-    new_price: 85000,
-    old_price: 100000,
-    offer: false,
-  },
-  {
-    id: 407,
-    name: "SET MILAN - TONO BORGOÑA",
-    image: setmilanborgoña1,
-    new_price: 85000,
-    old_price: 100000,
-    offer: false,
-  },
+
 ];
 
 export default new_data;
