2f821a47ce1e770219f684d4f8e39168b93b46fe
Luis <trujillolopezluisxd@gmail.com>
2026-05-29 23:28:13 -0500

    oculatamos enterizos

--- src/Components/Assets/data.js
+++ src/Components/Assets/data.js
@@ -1,27 +1,27 @@
-import enterizoalmabeige1 from "./enterizos/enterizoalmabeige2.jpeg";
-import enterizoflexazul1 from "./enterizos/enterizoflexazul1.jpeg";
+// import enterizoalmabeige1 from "./enterizos/enterizoalmabeige2.jpeg";
+// import enterizoflexazul1 from "./enterizos/enterizoflexazul1.jpeg";
 import enterizoflexamarillo1 from "./enterizos/enterizoflexamarillo1.jpeg";
 import enterizomarfil1 from "./enterizos/enterizomarfil1.jpeg";
 
 let data_product = [
-  {
-    id: 101,
-    name: "ENTERIZO ALMA - TONO BEIGE",
-    category: "enterizos",
-    image: enterizoalmabeige1,
-    new_price: 50000,
-    old_price: 100000,
-    offer: true,
-  },
-  {
-    id: 119,
-    name: "ENTERIZO FLEX - TONO AZUL",
-    category: "enterizos",
-    image: enterizoflexazul1,
-    new_price: 90000,
-    old_price: 150000,
-    offer: true,
-  },
+  // {
+  //   id: 101,
+  //   name: "ENTERIZO ALMA - TONO BEIGE",
+  //   category: "enterizos",
+  //   image: enterizoalmabeige1,
+  //   new_price: 50000,
+  //   old_price: 100000,
+  //   offer: true,
+  // },
+  // {
+  //   id: 119,
+  //   name: "ENTERIZO FLEX - TONO AZUL",
+  //   category: "enterizos",
+  //   image: enterizoflexazul1,
+  //   new_price: 90000,
+  //   old_price: 150000,
+  //   offer: true,
+  // },
   {
     id: 106,
     name: "ENTERIZO FLEX - TONO AMARILLO",
