974b2659ee253132ea297d00498878120d4fcadf
Luis <trujillolopezluisxd@gmail.com>
2026-06-23 08:18:32 -0500

    ocultamos enterizo nerea 3

--- src/Components/Assets/data.js
+++ src/Components/Assets/data.js
@@ -1,15 +1,15 @@
-import enterizomarfil1 from "./enterizos/enterizomarfil1.jpeg";
+// import enterizomarfil1 from "./enterizos/enterizomarfil1.jpeg";
 
 let data_product = [
-  {
-    id: 102,
-    name: "ENTERIZO NEREA - TONO MARFIL",
-    category: "enterizos",
-    image: enterizomarfil1,
-    new_price: 85000,
-    old_price: 120000,
-    offer: true,
-  },
+  // {
+  //   id: 102,
+  //   name: "ENTERIZO NEREA - TONO MARFIL",
+  //   category: "enterizos",
+  //   image: enterizomarfil1,
+  //   new_price: 85000,
+  //   old_price: 120000,
+  //   offer: true,
+  // },
 ];
 
 export default data_product;
