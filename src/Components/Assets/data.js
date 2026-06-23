bd286cc51730f416504fe5a3fbfd638c05555b15
Luis <trujillolopezluisxd@gmail.com>
2026-06-23 08:25:01 -0500

    agregamos en fashion 2

--- src/Components/Assets/data.js
+++ src/Components/Assets/data.js
@@ -1,23 +1,23 @@
 import enterizopinkrosa1 from "./enterizos/enterizopinkrosa1.jpeg";
+import enterizobotaamarillo1 from "./enterizos/enterizobotaamarillo1.jpeg";
 
 let data_product = [
-  // {
-  //   id: 102,
-  //   name: "ENTERIZO NEREA - TONO MARFIL",
-  //   category: "enterizos",
-  //   image: enterizomarfil1,
-  //   new_price: 85000,
-  //   old_price: 120000,
-  //   offer: true,
-  // },
-
-    {
+  {
     id: 107,
     name: "ENTERIZO PINK - TONO ROSA",
     category: "enterizos",
     image: enterizopinkrosa1,
     new_price: 115000,
     old_price: 120000,
+    offer: false,
+  },
+    {
+    id: 108,
+    name: "ENTERIZO BOTA CAMPANA - TONO AMARILLO",
+    category: "enterizos",
+    image: enterizobotaamarillo1,
+    new_price: 135000,
+    old_price: 140000,
     offer: false,
   },
 ];
