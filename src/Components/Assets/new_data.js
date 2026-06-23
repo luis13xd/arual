3f7e8d194d0090704773c444df1fdb5d71514f4a
Luis <trujillolopezluisxd@gmail.com>
2026-06-23 08:26:21 -0500

    agregamos en new collection

--- src/Components/Assets/new_data.js
+++ src/Components/Assets/new_data.js
@@ -1,16 +1,25 @@
-// import enterizomarfil1 from "./enterizos/enterizomarfil1.jpeg";
+import enterizopinkrosa1 from "./enterizos/enterizopinkrosa1.jpeg";
+import enterizobotaamarillo1 from "./enterizos/enterizobotaamarillo1.jpeg";
 
 let new_data = [
-  // {
-  //   id: 102,
-  //   name: "ENTERIZO NEREA - TONO MARFIL",
-  //   category: "enterizos",
-  //   image: enterizomarfil1,
-  //   new_price: 85000,
-  //   old_price: 120000,
-  //   offer: true,
-  // },
-
+  {
+    id: 107,
+    name: "ENTERIZO PINK - TONO ROSA",
+    category: "enterizos",
+    image: enterizopinkrosa1,
+    new_price: 115000,
+    old_price: 120000,
+    offer: false,
+  },
+  {
+    id: 108,
+    name: "ENTERIZO BOTA CAMPANA - TONO AMARILLO",
+    category: "enterizos",
+    image: enterizobotaamarillo1,
+    new_price: 135000,
+    old_price: 140000,
+    offer: false,
+  },
 ];
 
 export default new_data;
